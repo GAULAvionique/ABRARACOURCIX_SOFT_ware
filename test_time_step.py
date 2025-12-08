43a198faefd6c6c1755c5cd8f77443c7091fb09d
remi <remplante@hotmail.com>
2025-12-08 00:14:17 -0500

    ajout support vitesse négative

--- test_time_step.py
+++ test_time_step.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-file_path = "./data_h15_test.csv"
+file_path = "./data/data_CDC_asserv.csv"
 
 df = pd.read_csv(file_path, index_col=False)
 
 times = df["time"].to_numpy()
 speed = df["Speed"].to_numpy()
-servo = df["Servo"].to_numpy()
+servo = df["setpoint"].to_numpy()
 
 print(speed)
 plt.plot(times, speed)
